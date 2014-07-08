c67f39fd822e8cac0d9a02a180bcf84eea72dfb8
hungrycat <rgoldfinger@gmail.com>
2014-07-07 23:30:36 -0700

    s

--- js/components/Pick.react.js
+++ js/components/Pick.react.js
@@ -12,6 +12,8 @@ var addMap = function(lat, lon) {
         maxZoom: 18
       }).addTo(map);
 
+      // map.on()
+
 };
 
 var Pick = React.createClass({
@@ -29,21 +31,19 @@ var Pick = React.createClass({
       addMap(lat, lon);
       
     }
-    //https://a.tiles.mapbox.com/v4/rgoldfinger.ind9i047/page.html?access_token=pk.eyJ1IjoicmdvbGRmaW5nZXIiLCJhIjoiNmpLM1pGcyJ9.yEeB7kkHxdZ5tWwBEissBA
-    //access_token=pk.eyJ1IjoicmdvbGRmaW5nZXIiLCJhIjoiNmpLM1pGcyJ9.yEeB7kkHxdZ5tWwBEissBA#4/37.77/-122.42
 
-    // <iframe width='100%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v4/rgoldfinger.ind9i047/attribution,zoompan.html?access_token=pk.eyJ1IjoicmdvbGRmaW5nZXIiLCJhIjoiNmpLM1pGcyJ9.yEeB7kkHxdZ5tWwBEissBA'></iframe>
   },
 
   handleGo: function() {
-    //
+    console.log();
+    var name = prompt("name:");
   },
 
   render: function() {
     return (
       <div className="question"
         // onTouchEnd={this.handleGo}
-        onClick={this.handleGo}>
+        // onClick={this.handleGo}>
         <div id="map" className="picker-map"></div>
       </div>
     )
